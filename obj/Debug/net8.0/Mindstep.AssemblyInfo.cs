dcdcbf7af6d61c1ae9b17d42217879ddd7833649
Isabele Leal <isabeleleal55@gmail.com>
2024-11-22 16:34:42 -0300

    Atualizacao

--- obj/Debug/net8.0/Mindstep.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mindstep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mindstep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7816cfec360e509fea4d48583a7f64dfc89f2d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3188fe22101aac95ad51150d4321b6955f40b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mindstep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mindstep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
