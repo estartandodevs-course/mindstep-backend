c01b4bc09f4c03f5912abffd19adac17539b5bb9
Isabele Leal <103459827+isabeleleal@users.noreply.github.com>
2024-11-25 16:09:48 -0300

    Atualizacao Usuarios

--- src/contas/mindstep.contas.app/domain/Usuario.cs
+++ src/contas/mindstep.contas.app/domain/Usuario.cs
@@ -5,5 +5,16 @@ namespace mindstep.contas.app.domain;
 
 public class Usuario : Entity, IAggregateRoot
 {
-    public int MyProperty { get; set; 
+    public String Nome { get;  set; }
+    public String Email { get; set; }   
+
+    public DateTime DataNascimento { get; set; }
+    public int Celular { get; set; }
+
+    public String Estado { get; set; } 
+
+    public String Cidade { get; set; }
+
+    public String Formacao { get; set; }
+
 }
