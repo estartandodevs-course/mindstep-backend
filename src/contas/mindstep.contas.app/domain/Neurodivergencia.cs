47ea0d9f927a0a42e86bcd4ab1fb527d1fa71ac4
Isabele Leal <103459827+isabeleleal@users.noreply.github.com>
2024-11-25 23:16:49 -0300

    Atualizacao Neurodivergencia

--- src/contas/mindstep.contas.app/domain/Neurodivergencia.cs
+++ src/contas/mindstep.contas.app/domain/Neurodivergencia.cs
@@ -1,6 +1,17 @@
+using EstartandoDevsCore.DomainObjects;
+
 namespace mindstep.contas.app.domain;
 
-public class Neurodivergencia
+public class Neurodivergencia : Entity
 {
+    public string DesafioDeAprendizado{ get; set; }
 
+    public string AtividadesAjudamConcentrar { get; set; }
+
+    public bool IntrucaoDetalhadaOuParcialDireta{ get; set; }
+
+    public bool PossuiLaudoNeurodivergente { get; set; }
+
+    public bool JaUsouFerramentasDeApoio { get; set; }
+    public bool FerramentasUsadasUteis { get; set; }
 }
