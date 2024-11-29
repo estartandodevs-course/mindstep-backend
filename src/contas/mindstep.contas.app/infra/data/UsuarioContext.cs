6f64532fa1dfb4461cb5ff9c1841541995f75074
Alan Santana <72580528+gankun26@users.noreply.github.com>
2024-11-29 00:07:50 -0300

    feat: criando contexto de usuario

--- src/contas/mindstep.contas.app/infra/data/UsuarioContext.cs
+++ src/contas/mindstep.contas.app/infra/data/UsuarioContext.cs
@@ -1,9 +1,33 @@
 using System;
 using FluentValidation.Results;
 using Microsoft.EntityFrameworkCore;
+using mindstep.contas.app.domain;
 
 
 public class UsuarioContext : DbContext
 {
-    
+    public UsuarioContext(DbContextOptions<UsuarioContext> options) : base(options)
+    {}
+
+    public DbSet<Usuario> Usuarios {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Usuario>(entity =>{
+            entity.HasKey(x => x.Id);
+            entity.Property(u => u.Celular);
+            entity.Property(u => u.Estado);
+            entity.Property(u => u.Cidade);
+            entity.Property(u => u.DataDeAlteracao);
+            entity.Property(u => u.DataDeCadastro);
+            entity.Property(u => u.DataNascimento);
+            entity.Property(u => u.Estado);
+            entity.Property(u => u.TipoUsuario);
+            entity.Property(u => u.Notificacoes);
+            entity.Property(u => u.Neurodivergencia);
+            entity.Property(u => u.Login);
+        });
+    }
 }
