49dceeae4026344a1a87134cd4b7007272542c28
Alan Santana <72580528+gankun26@users.noreply.github.com>
2024-12-08 23:18:26 -0300

    feat: adicionado mapping de usuario

--- src/contas/mindstep.contas.app/infra/Mappings/UsuarioMappings.cs
+++ src/contas/mindstep.contas.app/infra/Mappings/UsuarioMappings.cs
@@ -11,6 +11,36 @@ namespace mindstep.contas.app.infra.Mappings
         {
             builder.ToTable("Usuarios");
             builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome).HasColumnName("Nome").HasMaxLength(300).IsRequired();
+            builder.Property(x => x.TipoUsuario).HasColumnName("TipoUsuario").HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Celular).HasColumnName("Celular").HasMaxLength(100);
+            builder.Property(x => x.DataNascimento).HasColumnName("DataNascimento").HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Formacao).HasColumnName("Formacao").HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Foto).HasColumnName("Foto").HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Neurodivergencia).HasColumnName("Neurodivergencia").HasMaxLength(100).IsRequired();
+
+            builder.OwnsOne(x => x.Login, login =>
+            {
+                login.Property(x => x.Hash).HasColumnName("LoginHash");
+
+                login.OwnsOne(x => x.Email, email =>{
+                    email.Property(x => x.Endereco).HasMaxLength(500).HasColumnName("Email");
+                });
+
+                login.OwnsOne(x => x.Senha, senha =>{
+                    senha.Property(x => x.Valor).HasMaxLength(100).HasColumnName("Senha");
+                });
+            });
+
+            builder.OwnsMany(x => x.Enderecos, endereco =>
+            {
+                endereco.WithOwner().HasForeignKey("UsuarioId");
+                endereco.Property<Guid>("Id").ValueGeneratedOnAdd();
+                endereco.HasKey("Id");
+
+                endereco.Property(x => x.Estado).HasMaxLength(100);
+                endereco.Property(x => x.Cidade).HasMaxLength(100);
+            });
         }
     }
 }
