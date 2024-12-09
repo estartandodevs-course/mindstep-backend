cf5c20e2ad3d6e07a3ebb865712f878ba7318ad6
Alan Santana <72580528+gankun26@users.noreply.github.com>
2024-12-09 01:22:27 -0300

    feat: criando configuracao de conexao com banco

---
+++ src/api/mindstep.api/Configuration/ApiConfig.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mindstep.api.Configuration
+{
+    public static  class ApiConfig
+    {
+        
+    }
+}
