ec3c14b12236b5e67603d2204c94656536b60e9d
Alan Santana <72580528+gankun26@users.noreply.github.com>
2024-12-09 01:12:50 -0300

    feat: adicionado mais conteudo ao contexto de usuario

--- src/contas/mindstep.contas.app/infra/data/UsuarioContext.cs
+++ src/contas/mindstep.contas.app/infra/data/UsuarioContext.cs
@@ -19,13 +19,14 @@ public class UsuarioContext : DbContext
             entity.HasKey(x => x.Id);
             entity.Property(u => u.Celular);
             entity.Property(u => u.Enderecos);
-            entity.Property(u => u.DataDeAlteracao);
-            entity.Property(u => u.DataDeCadastro);
             entity.Property(u => u.DataNascimento);
             entity.Property(u => u.TipoUsuario);
             entity.Property(u => u.Notificacoes);
             entity.Property(u => u.Neurodivergencia);
             entity.Property(u => u.Login);
+            entity.Property(u => u.Nome);
+            entity.Property(u => u.Foto);
+            entity.Property(u => u.Formacao);
         });
     }
 }
