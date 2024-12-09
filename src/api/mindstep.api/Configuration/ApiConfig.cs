8d1a4419cd025796541a035bbab39ba27bcf1de2
Alan Santana <72580528+gankun26@users.noreply.github.com>
2024-12-09 01:51:45 -0300

    feat: adicionando configuracao com banco

--- src/api/mindstep.api/Configuration/ApiConfig.cs
+++ src/api/mindstep.api/Configuration/ApiConfig.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace mindstep.api.Configuration
 {
     public static  class ApiConfig
     {
-        
+        private const string ConexaoBanco = "MindStepConnection";
+
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<UsuarioContext>
+        }
     }
 }
