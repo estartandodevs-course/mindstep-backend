837f6525198580d2d5abfc09b4ac24ed015200db
Alan Santana <72580528+gankun26@users.noreply.github.com>
2024-12-13 22:58:10 -0300

    feat: criando model de usuario

--- obj/Debug/net8.0/Mindstep.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mindstep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mindstep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7816cfec360e509fea4d48583a7f64dfc89f2d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc365a2ae582d9fdc09b8118835dbfe6c42f60b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mindstep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mindstep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Models/UsuarioModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mindstep_backend.src.Models
+{
+    public class UsuarioModel
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public int Celular { get; private set; }
+        public string Formacao { get; private set; }
+        public string TipoUsuario { get; private set; }
+        public string Neurodivergencia { get; private set; }
+        public string Foto { get; private set; }
+    }
+}
